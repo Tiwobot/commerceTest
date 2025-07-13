bea4b90e39cfaa86718f5dde458f40722e98ece2
Tugan <tuganatila@gmail.com>
2025-07-13 13:17:18 +0300

    Add heading elements for improved accessibility and SEO on the Home page. Include GeistSans font for enhanced typography.

--- yangfinity/src/app/page.tsx
+++ yangfinity/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { ThreeItemGrid } from '../../components/grid/three-items';
 import { Carousel } from '../../components/carousel';
+import { GeistSans } from 'geist/font/sans';
 
 export default function Home() {
   return (
     <>
+      <h1 className="sr-only">Buy Yang for Metin2 - Yangfinity</h1>
+      <h2 className={`sr-only ${GeistSans.className}`}>Buy Yang for Metin2 Private Servers at <span className="text-[#b6e700]">Yangfinity</span></h2>
       <ThreeItemGrid />
       <Carousel />
     </>
