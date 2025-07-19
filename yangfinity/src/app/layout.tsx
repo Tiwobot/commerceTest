e49fe9aa9390416a2fe94bb44686c88593f25b52
Tugan <tuganatila@gmail.com>
2025-07-19 14:33:14 +0300

    Add Trustpilot verification script to layout for enhanced user engagement and crawler detection.

--- yangfinity/src/app/layout.tsx
+++ yangfinity/src/app/layout.tsx
@@ -75,6 +75,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
               'query-input': 'required name=search_term_string'
             }
           }) }} />
+          {/* Trustpilot Verification Script (plain for crawler detection) */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+                var a=d.createElement(s);a.async=1;a.src=r;a.type='text/javascript';var f=d.getElementsByTagName(s)[0];
+                f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+                tp('register', '049PdqiRfjj2TNGr');
+              `
+            }}
+          />
           {/* Google Analytics (GA4) */}
           <Script id="ga-script" strategy="afterInteractive">
             {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-XXXXXXXXXX');`}
